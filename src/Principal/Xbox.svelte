1a5772093f8224efe12ebac5ff2f7ba7b29e3c87
diego4521 <diego_arciniega_t@encrym.edu.mx>
2023-07-31 09:27:52 -0600

    ..

---
+++ src/Principal/Xbox.svelte
@@ -0,0 +1,103 @@
+
+
+
+<div class=row>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\halo.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> HALO REACH</h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\gears.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> GEARS OF WAR 4 </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\forza23.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> FORZA </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\elder.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body ">
+            <h5 class="card-title"> ELDEN RING </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col" style="margin-top: 40px;"></div>
+</div>
+<div class=row>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\persona5.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> PERSONA 5 </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\red.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> RED DEAD REDEMPTION II </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\sekiro.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> SEKIRO </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+
+<style>
+.btn-primary{
+  padding: 2rem;
+  background-color: green;
+  border-color: black; 
+  }
+
+  .mevale{
+    height: 18rem;
+
+  }
+  
+
+</style>

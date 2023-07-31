0db459bfb49be2d7781079efbe0c42e6bf218da8
Alexei140602 <133258992+Alexei140602@users.noreply.github.com>
2023-07-31 08:34:00 -0600

    add

---
+++ src/Principal/Nintendo.svelte
@@ -0,0 +1,100 @@
+<div class=row>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#/breath>
+            <img src="statics\breath.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> TLOZ:Breath of the Wild </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\mario_kart.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> Mario Kart 8 </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\smash.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> Super Smash Bros </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\odyssey.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body ">
+            <h5 class="card-title"> Mario Odyssey </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col" style="margin-top: 40px;"></div>
+</div>
+<div class=row>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\persona5.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> PERSONA 5 </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\three.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title">Fire Emblem </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+    <div class=col>
+        <div class="btn btn-primary" style="width: 22rem;">
+            <a href=#>
+            <img src="statics\crossing.jpg" class="card-img-top mevale" alt="..."></a>
+            <div class="card-body">
+            <h5 class="card-title"> Animal Crossing: New Horizons </h5>
+            <p class="card-text"> $sinko peso </p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+
+<style>
+.btn-primary{
+  padding: 2rem;
+  background-color: rgb(255, 0, 0);
+  border-color: black; 
+  }
+
+  .mevale{
+    height: 18rem;
+
+  }
+  
+
+</style>
